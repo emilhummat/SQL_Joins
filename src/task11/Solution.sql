10273ef94ea87eedbea1210044fbfaad67ba96ee
emilhummat <emilhummat@gmail.com>
2021-12-19 16:48:45 -0500

    Task 11

--- src/task11/Solution.sql
+++ src/task11/Solution.sql
@@ -1,2 +1,7 @@
 --Write a query in SQL to display the full name (firt and last name ) of employee
 --with ID and name of the country presently where (s)he is working.
+select e.employee_id, e.first_name, e.last_name, c.country_id, c.country_name
+from employees e join departments d on e.department_id = d.department_id
+    join locations loc on loc.location_id = d.location_id
+    join countries c on c.country_id = loc.country_id
+group by (e.employee_id, e.first_name, e.last_name, c.country_id, c.country_name);
