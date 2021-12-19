9b0326f2539d768435ef499a5d450f09123c8b82
emilhummat <emilhummat@gmail.com>
2021-12-19 17:49:37 -0500

    Task 15

--- src/task15/Solution.sql
+++ src/task15/Solution.sql
@@ -1,1 +1,4 @@
 -- Display department name, average salary and number of employees with commission within the department.
+SELECT d.department_name, ROUND(avg(e.salary)), count(e.commission_pct) as EMPLOYEE_WITH_COMMISSION
+FROM employees e join departments d on e.department_id=d.department_id
+group by (d.department_name)
