b286a94328a42d33eca84d3a28477e81942d84fb
emilhummat <emilhummat@gmail.com>
2021-12-19 17:35:24 -0500

    Task 14

--- src/task14/Solution.sql
+++ src/task14/Solution.sql
@@ -1,1 +1,3 @@
 --Display employee ID, first name, last name and the date on which he ended his previous job.
+SELECT  e.employee_id , e.first_name, e.last_name, h.end_date
+FROM employees e JOIN job_history h ON e.employee_id=h.employee_id
