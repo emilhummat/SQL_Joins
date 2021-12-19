72e869b22d4cd4814fb6927f6e8bbd48e24d5794
emilhummat <emilhummat@gmail.com>
2021-12-18 19:48:59 -0500

    Task 10

--- src/task10/Solution.sql
+++ src/task10/Solution.sql
@@ -1,2 +1,6 @@
 --Write a query in SQL to display full name(first and last name), job title, starting and ending date of
 --last jobs for those employees who worked without a commission percentage.
+Select e.employee_id, e.last_name, e.first_name, h.start_date, h.end_date, j.job_title
+from employees e join job_history h on e.employee_id=h.employee_id
+join jobs j on h.job_id=j.job_id
+where e.commission_pct is null;
