d522dbb473040fa744d7a7bb401927db2390e3dc
emilhummat <emilhummat@gmail.com>
2021-12-19 23:25:00 -0500

    Task 2

--- src/task2/Solution.sql
+++ src/task2/Solution.sql
@@ -1,1 +1,6 @@
 --Display the departments into which no employee joined in last two years.
+select d.department_id, d.department_name
+from departments d join employees e on d.department_id=e.department_id
+where extract(year from hire_date)<>2020 and extract(year from hire_date)<>2021
+group by d.department_id, d.department_name
+order by department_id
