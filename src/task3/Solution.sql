19c2119954d8ca19847a9d5d9d7367375d1ab491
emilhummat <emilhummat@gmail.com>
2021-12-19 22:57:07 -0500

    Task 3

--- src/task3/Solution.sql
+++ src/task3/Solution.sql
@@ -1,1 +1,6 @@
 --Display department ID, name, year, and Number of employees joined.
+
+select d.department_id, d.department_name, extract(year from hire_date) as year, count(e.employee_id)
+from departments d join employees e on d.department_id=e.department_id
+group by extract(year from hire_date),d.department_id, d.department_name
+order by department_id
